571b2c49384b73786589b6f3b9594bc4da7d8ecc
Francois Ribalet <ribalet@uw.edu>
2018-03-02 13:32:35 -0800

    remove data pulling from AWS, replaced by DAT

--- d1d2fsc-beads.R
+++ d1d2fsc-beads.R
@@ -4,42 +4,15 @@ library(googlesheets)
 
 
 
-setwd("~/Documents/DATA/Codes/")
+setwd("~/Documents/DATA/Codes/seaflow-filter")
+
 
 ################################################
-### Copy EVT files from AWS to local machine ###
+### Copy EVT files from DAT to local machine ###
 ################################################
+# link to EVT folder containing EVT files use with this script:
 
-system("aws s3 ls s3://armbrustlab.seaflow/")
-
-
-#list of SEAFLOW cruise from official LOG
- x <- gs_title("SeaFlow\ instrument\ log", verbose = TRUE)
-list <- gs_read(x)
-cruise.list <- list$cruise
-
-
-### COPY FROM AWS TO LOCAL
-for(cruise in cruise.list){
-  print(cruise)
-
-  # Get the list of ALL EVT files from a specifc cruise
-  files <- get_bucket(bucket = 'armbrustlab.seaflow', prefix=paste0(cruise,"/"), max=Inf) # if it doesn't work, try opening S3 on a browser
-
-  if(length(files) == 0){
-    print(paste0("no EVT files found for cruise ", cruise, ", check that cruise name in official LOG matches the one in AWS"))
-    next
-    }
-    ##########################
-    # select every 100 files #
-    ##########################
-    id <- seq(30, length(files), by=100)
-
-    # COPY evt files from AWS to local
-  for(i in id) system(paste0("aws s3 cp s3://armbrustlab.seaflow/",files[i]$Contents$Key,"seaflow-filter",files[i]$Contents$Key))
-
-}
-
+dat://2b03e08a5cc4b4cc430d5a355115c6a1597ca6d6ccfc9778f1fb5b545a600deb
 
 
 ####################################
@@ -53,19 +26,15 @@ cruise.list <- list$cruise
 
 for(cruise in cruise.list){
   print(cruise)
-  evt.list <- list.files(path=paste0("~/Desktop/EVT_allcruises/",cruise), pattern=".gz", recursive=T, full.names=T)
-
-
-DF <- concatenate.evtopp(evt.list, n=100000, min.fsc = 2, min.pe =5, min.chl=0, transform=T)
-
-
-write.csv(DF, paste0("seaflow-filter",cruise,"/concatenated_EVT.csv"), quote=F, row.names=F)
+  evt.list <- list.files(path=paste0("evt/",cruise), pattern=".gz", recursive=T, full.names=T)
+  DF <- concatenate.evtopp(evt.list, n=100000, min.fsc = 2, min.pe =5, min.chl=0, transform=T)
+  write.csv(DF, paste0(cruise,"/concatenated_EVT.csv"), quote=F, row.names=F)
 
 }
 
 
 # Check EVT cytograms
-  DF <- read.csv(paste0("seaflow-filter",cruise,"/concatenated_EVT.csv"))
+  DF <- read.csv(paste0(cruise,"/concatenated_EVT.csv"))
   par(mfrow=c(2,3))
   plot.cytogram(DF,'D1',"D2")
   plot.cytogram(DF,'fsc_small',"D1")
@@ -80,10 +49,12 @@ write.csv(DF, paste0("seaflow-filter",cruise,"/concatenated_EVT.csv"), quote=F, 
 #########################################################
 cruise <- cruise.list[49]
   print(cruise)
-  DF <- read.csv(paste0("seaflow-filter",cruise,"/concatenated_EVT.csv"))
+  DF <- read.csv(paste0(cruise,"/concatenated_EVT.csv"))
+
+  # Gates beads to find intersections of the two slopes used for OPP filtration
   ip <- inflection.point(DF)
 
-  write.csv(data.frame(cruise, ip), paste0("seaflow-filter",cruise,"/d1d2fsc-filterparams.csv"),quote=F, row.names=F)
+  write.csv(data.frame(cruise, ip), paste0(cruise,"/d1d2fsc-filterparams.csv"),quote=F, row.names=F)
 
 
 
@@ -92,7 +63,7 @@ cruise <- cruise.list[49]
 ######################################################################
 ### CONCATENATE d1d2fsc-filterparams.csv from ALL cruises TOGETHER ###
 ######################################################################
-csv.list <- list.files(path="seaflow-filter", pattern="d1d2fsc-filterparams.csv", recursive=T, full.names=T)
+csv.list <- list.files(path=".", pattern="d1d2fsc-filterparams.csv", recursive=T, full.names=T)
 
 DF <- NULL
 for(file in csv.list){
@@ -101,9 +72,9 @@ for(file in csv.list){
    DF <- rbind(DF,df)
   }
 
-write.csv(DF,"seaflow-filter/ALL-filterparams.csv", quote=F, row.names=F)
+write.csv(DF,"ALL-filterparams.csv", quote=F, row.names=F)
 
-png("seaflow-filter/ALL-filterparams.png",width=20, height=30, unit='in', res=100)
+png("ALL-filterparams.png",width=20, height=30, unit='in', res=100)
 
 layout(matrix(c(1,2,3,3), 2, 2, byrow = TRUE))
 par(pty='m', oma=c(0,5,0,0),cex=1.4)
