f3cb657e8c1bf0b99025568512328a079d49a668
Francois Ribalet <ribalet@uw.edu>
2018-09-28 10:13:56 -0700

    change plot

--- d1d2fsc-beads.R
+++ d1d2fsc-beads.R
@@ -1,24 +1,5 @@
 library(popcycle)
 
-offset <- function(inst, fsc, d1, d2){
-      fsc <- as.numeric(fsc)
-      d1<- as.numeric(d1)
-      d2 <- as.numeric(d2)
-      slopes <- read.csv(system.file("filter", "seaflow_filter_slopes.csv",package='popcycle'))
-      slopes[slopes$ins== inst, 'notch.small.D1']
-      notch.small.D1 <- slopes[slopes$ins== inst, 'notch.small.D1']
-      notch.small.D2 <- slopes[slopes$ins== inst, 'notch.small.D2']
-      offset.small.D1 <- round(d1 - notch.small.D1 * fsc)
-      offset.small.D2 <- round(d2 - notch.small.D2 * fsc)
-      notch.large.D1 <- slopes[slopes$ins== inst, 'notch.large.D1']
-      notch.large.D2 <- slopes[slopes$ins== inst, 'notch.large.D2']
-      offset.large.D1 <- round(d1 - notch.large.D1 * fsc)
-      offset.large.D2 <- round(d2 - notch.large.D2 * fsc)
-
-      return(c(offset.small.D1,offset.small.D2,offset.large.D1,offset.large.D2, notch.small.D1,notch.small.D2, notch.large.D1, notch.large.D2))
-      }
-
-
 create.filter.params <- function(inst, fsc, d1, d2, width=3000, slope.file=NULL) {
   QUANTILES <- c(2.5, 50.0, 97.5)
 
@@ -128,7 +109,7 @@ DF <- concatenate.evtopp(evt.list, n=100000, min.fsc = 25000, min.pe =25000, min
 ################################################################################
 cruise.list <- list.files("~/Documents/DATA/Codes/seaflow-sfl/curated/", pattern='.sfl',full.names = F)
 
-  #i <- 32
+  i <- 17
   exp <- unlist(list(strsplit(cruise.list[i],"_")))
   if(length(exp) > 2) { cruise <- paste(exp[1],exp[2],sep="_")
   } else if(length(exp) ==2) cruise <- exp[1]
@@ -144,7 +125,7 @@ cruise.list <- list.files("~/Documents/DATA/Codes/seaflow-sfl/curated/", pattern
   # check OPP filtration
   evt.list <- list.files(path=paste0(path.to.data,cruise), pattern=".gz", recursive=T, full.names=T)
   evt <- readSeaflow(evt.list[length(evt.list)/2],transform=F)
-  #  evt <- readSeaflow(evt.list[34],transform=F)
+  #  evt <- readSeaflow(evt.list[1],transform=F)
 
   filter.params <- create.filter.params(inst, fsc=ip$fsc, d1=ip$d1, d2=ip$d2, width=5000)
     #filter.params$notch.small.D1 <- filter.params$notch.small.D2 <- 1000
@@ -152,14 +133,15 @@ cruise.list <- list.files("~/Documents/DATA/Codes/seaflow-sfl/curated/", pattern
 
     opp <- filter.notch(evt, filter.params[2,])$opp
     plot.cytogram(opp, "fsc_small", "pe")
-    b <- subset(opp, pe > 50000)
+    b <- subset(opp, pe > 40000)
     plot.cytogram(b, "fsc_small", "D1")
+    plot.cytogram(b, "fsc_small", "D2")
 
 
   # only if satisfied with filter params
   write.csv(data.frame(instrument=inst, cruise, ip), paste0(cruise,"/d1d2fsc.csv"),quote=F, row.names=F)
 
-}
+
 
 
 ######################################################################
@@ -186,26 +168,15 @@ write.csv(DF,"ALL-d1d2fsc.csv", quote=F, row.names=F)
 ### DOWLOAD FILTER SLOPES
 slope.file <- "https://raw.githubusercontent.com/armbrustlab/seaflow-virtualcore/master/1.bead_calibration/seaflow_filter_slopes.csv"
 
-x <- gs_title("SeaFlow\ instrument\ log", verbose = TRUE)
-list <- gs_read(x)
-DF <- read.csv("ALL-d1d2fsc.csv")
-DF <- merge(DF, list[,c("cruise","instrument","year")])
-
-
-@@@@@@@@@@@@@@@@@@@@@@@
-
+params <- NULL
 for( c in unique(DF$cruise)){
-c <- "SCOPE_1"
-df <- subset(DF, cruise  == c)
-p <- create.filter.params(inst=unique(df["instrument"]), fsc=df["fsc"], d1=df["d1"], d2=df["d2"], width=5000)
-params <- rbind(params, p)
-
-}
-
-@@@@@@@@@@@@@@@@@@@@@@@
-params <- t(apply(DF,1, function(DF) create.filter.params(inst=DF["instrument"], fsc=DF["fsc"], d1=DF["d1"], d2=DF["d2"], width=5000)))
-params <- do.call(rbind.data.frame, params)
-params$cruise <- rep(DF$cruise, each=3)
+  #c <- "SCOPE_1"
+  df <- subset(DF, cruise  == c)
+  p <- create.filter.params(inst=unique(df[,"instrument"]), fsc=df[,"fsc"], d1=df[,"d1"], d2=df[,"d2"], width=5000)
+  p$cruise <- c
+  p$instrument <- unique(df[,"instrument"])
+  params <- rbind(params, p)
+  }
 
 write.csv(params,"ALL-filterparams.csv", quote=F, row.names=F)
 
@@ -219,23 +190,14 @@ write.csv(params,"ALL-filterparams.csv", quote=F, row.names=F)
 ##############################
 library(scales)
 cols <- colorRampPalette(c("blue4","royalblue4","deepskyblue3", "seagreen3", "yellow", "orangered2","darkred"))
-DF <- DF[order(DF$year),]
-
-df <- as.data.frame(t(apply(DF,1, function(DF) offset(inst=DF["instrument"], fsc=DF["fsc"], d1=DF["d1"], d2=DF["d2"]))))
-colnames(df) <- c("offset.small.D1","offset.small.D2","offset.large.D1","offset.large.D2", "notch.small.D1","notch.small.D2", "notch.large.D1", "notch.large.D2")
-df$cruise <- as.vector(DF$cruise)
+DF <- read.csv("ALL-filterparams.csv")
+DF1 <- subset(DF, quantile == 50.0)
+DF2 <- subset(DF, quantile != 50.0)
 
 png("ALL-d1d2fsc.png",width=12, height=12, unit='in', res=400)
 
-plot(c(0,2^16), c(0,2^16), pch=NA, xlab="fsc_small", ylab="D1 & D2",las=1, main='Bead coordinates')
-  for(i in 1:nrow(df)){
-      abline(b=mean(df$notch.small.D1[i],df$notch.small.D2[i]), a=mean(df$offset.small.D1[i],df$notch.small.D2[i]),col='grey',lty=2)
-      abline(b=mean(df$notch.large.D1[i],df$notch.large.D2[i]), a=mean(df$offset.large.D1[i],df$notch.large.D2[i]),col='grey',lty=2)
-      points(DF[i,"fsc"],DF[i,"d1"],bg=alpha(cols(nrow(df))[i],0.5),pch=21,cex=2)
-    }
-    legend('topleft',legend=DF$cruise,pch=21, pt.bg=alpha(cols(nrow(df)),0.5), bty='n', ncol=2)
+plot(DF2[,"beads.fsc.small"], DF2[,"beads.D1"], col=alpha(cols(nrow(DF2)),0.5),pch=16,cex=1.5, xlab="fsc_small", ylab="D1 & D2",las=1, main='Bead coordinates', xlim=c(0,2^16), ylim=c(0,2^16))
+  points(DF1[,"beads.fsc.small"], DF1[,"beads.D1"], bg=alpha(cols(nrow(DF1)),0.5),pch=21,cex=2)
+  legend('topleft',legend=DF1$cruise,pch=21, pt.bg=alpha(cols(nrow(DF1)),0.5), bty='n', ncol=2)
 
 dev.off()
-
-id <- match(c("SCOPE_16","SCOPE_6","MBARI_1","DeepDOM"),DF$cruise)
-points(DF[id,"fsc"],DF[id,"d1"],pch=1,cex=3,col=2,lwd=2)
